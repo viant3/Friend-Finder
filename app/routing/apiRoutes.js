49edd0e15f984911cf8212a9bb079b6363550a63
victor politano <viant3ucf@gmail.com>
2019-09-12 16:27:04 -0400

    separated routes - still need logic and post route not working on apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,11 @@
 var friends = require("../data/friends");
 
+
 module.exports = function(app) {
 
     app.get("/api/friends", function(req, res) {
         return res.json(friends);
       });
     };
+
+   
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
-
+var friends = require('./app/data/friends.js')
 // Sets up the Express App
 // =============================================================
 var app = express();
@@ -20,46 +20,15 @@ app.use(express.json());
 // =============================================================
 
 
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
-// Basic route that sends the user first to the AJAX Page
+require('./app/routing/apiroutes.js')(app);
+require('./app/routing/htmlroutes.js')(app)
 
-
-// app.get("/add", function(req, res) {
-//   res.sendFile(path.join(__dirname, "add.html"));
-// });
-
-// app.get("/allchars", function(req, res) {
-//   res.sendFile(path.join(__dirname, "allchars.html"));
-// });
-
-// Displays all friends
-
-
-// Displays a single character, or returns false
-// app.get("/api/friends/:character", function(req, res) {
-//   var chosen = req.params.character;
-
-//   console.log(chosen);
-
-//   for (var i = 0; i < friends.length; i++) {
-//     if (chosen === friends[i].routeName) {
-//       return res.json(friends[i]);
-//     }
-//   }
-
-//   return res.json(false);
-// });
-
-// Create New friends - takes in JSON input
 app.post("/api/friends", function(req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
   var newFriend = req.body;
 
-  // Using a RegEx Pattern to remove spaces from newFriend
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+
 
   console.log(newFriend);
 
