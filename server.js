b5ca9bb160c188761020ed1f33ae97f12668b876
victor politano <viant3ucf@gmail.com>
2019-09-13 10:02:30 -0400

    added logic

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
-var friends = require('./app/data/friends.js')
+var bodyParser = require('body-parser');
+
 // Sets up the Express App
 // =============================================================
 var app = express();
@@ -12,30 +13,17 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//  friends (DATA)
-// =============================================================
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
 
 // Routes
 // =============================================================
 
 
-require('./app/routing/apiroutes.js')(app);
-require('./app/routing/htmlroutes.js')(app)
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app)
 
-app.post("/api/friends", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newFriend = req.body;
-
-
-
-  console.log(newFriend);
-
-  friends.push(newFriend);
-
-  res.json(newFriend);
-});
 
 // Starts the server to begin listening
 // =============================================================
